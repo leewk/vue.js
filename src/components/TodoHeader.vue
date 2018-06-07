c21c3a2767319a2e7f257bc7ff67bf71d1651053
leewk <dldnrud123@naver.com>
2018-06-07 22:38:51 +0900

    2018-06-07

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,5 +1,7 @@
 <template>
-	<div>header</div>
+	<header>
+		<h1>TODO it!</h1>
+	</header>
 </template>
 <script>
 
@@ -9,4 +11,22 @@ export default{
 </script>
 
 <style>
+body{
+text-align:center;
+background-color:#F6F6F8;
+}
+
+input{
+border-style: groove;
+width: 200px;
+}
+
+button{
+	border-style: groove;
+}
+
+.shadow{
+	box-shadow: 5px 10px 10px rgba(0,0,0,0.03)
+}
+
 </style>
