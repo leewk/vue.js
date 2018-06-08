16f045bc27a0a5cc4592358dee2cb0b02d53e843
leewk <dldnrud123@naver.com>
2018-06-09 01:26:27 +0900

    18-06-09

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,39 +1,27 @@
 <template>
 	<section>
-		<ul>
-			<li v-for="(todoItem,index) in todoItems" :ket="todoItem" class="shadow">
+		<transition-group name="list" tag ="ul">
+			<li v-for="(todoItem,index) in propsdata" :key="todoItem" class="shadow">
 				<i class="checkBtn fa fa-check" aria-hidden = "true"></i>
 				{{todoItem}}
 				<span class="removeBtn" type ="button" @click="removeTodo(todoItem, index)">
 					<i class="fa fa-trash" aria-hidden="true"></i>
 				</span>
 			</li>
-		</ul>
+		</transition-group>
 	</section>
 </template>
 <script>
 
 export default{
-	data(){
-		return {
-			todoItems:[]
-		}
-	},
-	created(){
-		if(localStorage.length > 0){
-			for(var i = 0; i < localStorage.length;i++){
-				this.todoItems.push(localStorage.key(i));
-
-
-			}
-		}
-	},
+	props:['propsdata'],
 	methods:{
 		removeTodo(todoItem, index){
 			localStorage.removeItem(todoItem);
-			this.todoItems.splice(index,1);
+			this.propsdata.splice(index,1);
 		}
 	}
+
 };
 </script>
 
@@ -66,4 +54,21 @@ li{
 	color: #de4343;
 }
 
+.list-item{
+	display: inline-block;
+	margin-right: 10px;
+}
+
+.list-move{
+	transition: transform 1s;
+}
+
+.list-enter-active, .list-leave-active{
+	transition: all 1s;
+}
+
+.list-enter, .list-leave-to{
+	opacity: 0;
+	transform: translateY(30px);
+}
 </style>
